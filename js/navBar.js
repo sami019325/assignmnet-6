c1e711b3b8f0c25f88490e2c7dbf9b212074ac38
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-09-04 02:30:52 +0600

    api connected successfully

--- js/navBar.js
+++ js/navBar.js
@@ -1,8 +1,8 @@
 const getData = category => {
 
-    fetch(`https://openapi.programming-hero.com/api/news/${category}`)
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category}`)
         .then(Response => Response.json())
-        .then(newData => showData(newData))
+        .then(newData => showData(newData, category))
 
 
 
@@ -13,7 +13,6 @@ getData('01')
 const newsTypeList = ['Home', 'Breaking news', 'Regular news', 'International news', 'Sports', 'Entertainment', 'Culture', 'Arts', 'All news'];
 
 newsTypeList.forEach(typeOfNews => {
-    console.log(typeOfNews);
     newsTypeContainer = document.getElementById('newsTypeContainer')
     const newsType = document.createElement('div');
     newsType.innerHTML = `
@@ -29,28 +28,54 @@ const displayNews = (newsType) => {
 
 
 // show data 
-const showData = datatype => { console.log(datatype); }
-/* <div class="p-2 bg-gray-50 rounded">
+const showData = (datatype, category) => {
+    const newsContainer = document.getElementById('news_container');
+    console.log(datatype.data, category)
+    const numberOfNews = datatype.data.length;
+    displayNumberOfNews(numberOfNews, category)
+    console.log("number of data:", numberOfNews)
+    const totalData = datatype.data
+    totalData.forEach(news => {
+        console.log(news.image_url)
+        const newsDiv = document.createElement('div');
+        newsDiv.innerHTML = `
+    <div class="p-2 mt-5 bg-gray-50 rounded">
 <div class="grid grid-cols-1 grid-rows-3 md:grid-cols-3 gap-4">
     <img class="col-span-1 w-full h-full row-span-3"
-        src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80"
+        src="${news.image_url}"
         alt="" srcset="">
     <div class="col-span-2 row-span-2 text-ellipsis">
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, minus!</h2>
+        <h2>${news.title}</h2>
         <p class=" h-24 text-ellipsis overflow-hidden">
         </p>
     </div>
     <div class="col-span-2 row-span-1 grid grid-cols-3">
-        <div class=" grid justify-items-center content-center">
-            <img src="image/Avatar.png" alt="" srcset="">
-            <p>jan 10, 2022</p>
+        <div class=" grid grid-cols-1 md:grid-cols-2 grid-rows-2 justify-items-center content-center">
+            <img class="w-10 h-10 rounded-full row-span-2" src="${news.author.img}" alt="" srcset="">
+            <p>${news.author.name}</p>
+            <p>${news.author.published_date.slice(0, 11)}</p>
         </div>
         <div class=" grid justify-items-center content-center">
-            <p class="font-bold text-gray-400">1.5M</p>
+            <p class="font-bold text-gray-400">${news.total_view} views</p>
         </div>
         <button class=" grid justify-items-center content-center">
             <img src="image/arrow-right-short.png" alt="" srcset="">
         </button>
     </div>
 </div>
-</div> */
+</div>
+    `
+        newsContainer.appendChild(newsDiv)
+    })
+}
+
+
+const displayNumberOfNews = (totalNumber, category) => {
+    const numberOfNewsContainer = document.getElementById('numberOfNews')
+    const numberOfNewsDiv = document.createElement('div')
+    numberOfNewsDiv.innerHTML = `
+    <div>${totalNumber} items found for ${category} </div>
+    `
+    numberOfNewsContainer.appendChild(numberOfNewsDiv)
+
+}
