088980b28c835fea49957fd3309be88fc444ab67
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-09-04 04:55:44 +0600

    loading js added

--- js/navBar.js
+++ js/navBar.js
@@ -9,6 +9,15 @@ const getData = (category, categoryName) => {
 
 }
 
+const displayNewsDetails = (id) => {
+    //textFull = undefined;
+    categoryName = "details";
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+        .then(Response => Response.json())
+        .then(newData => showData(newData, categoryName, textFull))
+        .catch(err => console.log(err));
+}
+
 
 const newsTypeList = ['Home', 'Breaking news', 'Regular news', 'International news', 'Sports', 'Entertainment', 'Culture', 'Arts', 'All news'];
 //for (typeOfNews in newsTypeList) {} 
@@ -21,19 +30,33 @@ newsTypeList.forEach(typeOfNews => {
     newsTypeContainer.appendChild(newsType)
 });
 
+const showLoading = (signal) => {
+    const loader = document.getElementById('loading')
+    console.log('loading', signal);
+
+    if (signal) {
+        loader.classList.remove('hidden');
+        document.getElementById('noNewsFound').style.display = 'none';
+    }
+    else {
+        loader.classList.add('hidden');
+    }
+}
+
+
 
 // show data 
-const showData = (datatype, categoryName) => {
+const showData = (datatype, categoryName, textFull = 150) => {
     const newsContainer = document.getElementById('news_container');
     newsContainer.innerHTML = "";
-    console.log(datatype.data, categoryName)
+    console.log("showData function:", datatype.data, categoryName)
     const numberOfNews = datatype.data.length;
+    if (numberOfNews === 0) { document.getElementById('noNewsFound').style.display = 'block'; }
     displayNumberOfNews(numberOfNews, categoryName)
     console.log("number of data:", numberOfNews)
     const totalData = datatype.data
-    console.log("dfsssssssssssssssssss", totalData)
     totalData.forEach(news => {
-        console.log(news.image_url)
+        console.log('Id of data: ', news._id)
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
     <div class="p-2 mt-5 bg-gray-50 rounded">
@@ -43,19 +66,19 @@ const showData = (datatype, categoryName) => {
         alt="" srcset="">
     <div class="col-span-2 row-span-2 text-ellipsis">
         <h2>${news.title}</h2>
-        <p class=" h-24 text-ellipsis overflow-hidden">
+        <p <span > ${news.details.slice(0, textFull)}</span>...
         </p>
     </div>
     <div class="col-span-2 row-span-1 grid grid-cols-3">
         <div class=" grid grid-cols-1 md:grid-cols-2 grid-rows-2 justify-items-center content-center">
             <img class="w-10 h-10 rounded-full row-span-2" src="${news.author.img}" alt="" srcset="">
             <p>${news.author.name}</p>
-            <p>${news.author.published_date.slice(0, 11)}</p>
+            <p>${news.author.published_date ? news.author.published_date.slice(0, 11) : 'no date available'}</p>
         </div>
         <div class=" grid justify-items-center content-center">
             <p class="font-bold text-gray-400">${news.total_view} views</p>
         </div>
-        <button class=" grid justify-items-center content-center">
+        <button onclick="displayNewsDetails('${news._id}','details')" class=" grid justify-items-center content-center">
             <img src="image/arrow-right-short.png" alt="" srcset="">
         </button>
     </div>
@@ -64,6 +87,7 @@ const showData = (datatype, categoryName) => {
     `
         newsContainer.appendChild(newsDiv)
     })
+    showLoading(false)
 };
 
 
@@ -79,6 +103,7 @@ const displayNumberOfNews = (totalNumber, category) => {
 
 
 const displayNews = (newsType) => {
+    showLoading(true)
     console.log(newsType);
     switch (newsType) {
         case "Home":
@@ -122,4 +147,5 @@ const displayNews = (newsType) => {
 };
 displayNews("Home")
 
-//getData('04')
+
+
